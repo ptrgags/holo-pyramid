7897e37b6665407433498e4694d2f49e89f11516
Peter Gagliardi <pcg34@drexel.edu>
2017-05-22 15:45:42 -0400

    Learn about cameras
    Learned how to add cameras and switch between them in Rajawali.

--- app/src/main/java/io/github/ptrgags/holopyramid/ExampleRenderer.kt
+++ app/src/main/java/io/github/ptrgags/holopyramid/ExampleRenderer.kt
@@ -1,12 +1,15 @@
 package io.github.ptrgags.holopyramid
 import android.content.Context
+import android.util.Log
+import android.view.KeyEvent
 import android.view.MotionEvent
+import org.rajawali3d.cameras.Camera
 import org.rajawali3d.lights.ALight
 import org.rajawali3d.lights.DirectionalLight
 import org.rajawali3d.materials.Material
 import org.rajawali3d.materials.methods.DiffuseMethod
 import org.rajawali3d.math.vector.Vector3
-import org.rajawali3d.primitives.Sphere
+import org.rajawali3d.primitives.Torus
 import org.rajawali3d.renderer.Renderer;
 
 /**
@@ -16,36 +19,71 @@ import org.rajawali3d.renderer.Renderer;
  * http://www.clintonmedbery.com/basic-rajawali3d-tutorial-for-android/
  */
 class ExampleRenderer : Renderer {
-    val sphere = Sphere(1.0f, 24, 24)
+    val model = Torus(1.0f, 0.5f, 20, 10);
+    var cameraNum = 0
 
     constructor(context: Context?): super(context) {
         setFrameRate(60)
     }
 
-    override fun onTouchEvent(event: MotionEvent?) {}
+    override fun onTouchEvent(event: MotionEvent?) {
+    }
+
+    fun onKeyUp(keyCode: Int, event: KeyEvent?) {
+        if (keyCode == KeyEvent.KEYCODE_BUTTON_A) {
+            cameraNum++
+            cameraNum %= currentScene.cameraCount
+            currentScene.switchCamera(cameraNum)
+        }
+    }
+
+    fun onKeyDown(keyCode: Int, event: KeyEvent?) {
+        if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT)
+            model.rotate(Vector3.Axis.Y, 3.0)
+        else if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT)
+            model.rotate(Vector3.Axis.Y, -3.0)
+    }
 
     override fun initScene() {
-        //Initialize the directional light
-        val directionalLight = DirectionalLight(1.0, 0.2, -1.0)
-        directionalLight.setColor(1.0f, 1.0f, 1.0f)
-        directionalLight.power = 2.0f
-        currentScene.addLight(directionalLight)
+        // Add two directional lights
+        val frontLight = DirectionalLight(1.0, 0.2, -1.0)
+        frontLight.setColor(0.5f, 0.0f, 1.0f)
+        frontLight.power = 2.0f
+        currentScene.addLight(frontLight)
+        val backLight = DirectionalLight(-1.0, 0.2, 1.0)
+        backLight.setColor(0.0f, 1.0f, 0.0f)
+        backLight.power = 2.0f
+        currentScene.addLight(backLight)
 
-        //Initialize our sphere light
+        //Material for shading the torus
         val material = Material()
         material.enableLighting(true)
         material.diffuseMethod = DiffuseMethod.Lambert()
-        material.color = 0xFF0000
+        material.color = 0xFFFFFF;
 
-        sphere.material = material
-        currentScene.addChild(sphere)
+        model.material = material
+        currentScene.addChild(model)
 
-        currentCamera.z = 4.2
-    }
+        //Add the four cameras
+        val frontCam = Camera()
+        frontCam.position = Vector3(0.0, 0.0, 5.0)
+        frontCam.lookAt = Vector3(0.0)
+        val leftCam = Camera()
+        leftCam.position = Vector3(5.0, 0.0, 0.0)
+        leftCam.lookAt = Vector3(0.0)
+        val backCam = Camera()
+        backCam.position = Vector3(0.0, 0.0, -5.0)
+        backCam.lookAt = Vector3(0.0)
+        val rightCam = Camera()
+        rightCam.position = Vector3(-5.0, 0.0, 0.0)
+        rightCam.lookAt = Vector3(0.0)
 
-    override fun onRender(ellapsedRealtime: Long, deltaTime: Double) {
-        super.onRender(ellapsedRealtime, deltaTime)
-        sphere.rotate(Vector3.Axis.Y, 1.0)
+        // Add the cameras to the scene
+        currentScene.clearCameras();
+        currentScene.addAndSwitchCamera(frontCam)
+        currentScene.addCamera(leftCam)
+        currentScene.addCamera(backCam)
+        currentScene.addCamera(rightCam)
     }
 
     override fun onOffsetsChanged(
@@ -54,6 +92,7 @@ class ExampleRenderer : Renderer {
             xOffsetStep: Float,
             yOffsetStep: Float,
             xPixelOffset: Int,
-            yPixelOffset: Int) {}
+            yPixelOffset: Int) {
+    }
 
 }
--- app/src/main/java/io/github/ptrgags/holopyramid/MainActivity.kt
+++ app/src/main/java/io/github/ptrgags/holopyramid/MainActivity.kt
@@ -1,13 +1,17 @@
 package io.github.ptrgags.holopyramid
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.ViewGroup
 import org.rajawali3d.view.ISurface
 import org.rajawali3d.view.SurfaceView
 
 class MainActivity : AppCompatActivity() {
+    var renderer: ExampleRenderer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +25,23 @@ class MainActivity : AppCompatActivity() {
                 surface,
                 ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT))
 
-        val renderer = ExampleRenderer(this);
+        renderer = ExampleRenderer(this)
         surface.setSurfaceRenderer(renderer)
     }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        renderer?.onTouchEvent(event)
+        return true
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        renderer?.onKeyUp(keyCode, event)
+        return true
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        renderer?.onKeyDown(keyCode, event)
+        return true
+    }
 }
+
